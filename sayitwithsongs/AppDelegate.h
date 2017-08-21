9a492cf19c34b796dba28120315b9aa5b6367bac
Haven Barnes <haven38@gmail.com>
2017-08-21 16:23:40 -0500

    Initial Commit

---
+++ sayitwithsongs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sayitwithsongs
+//
+//  Created by Haven Barnes on 8/21/17.
+//  Copyright © 2017 Azing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sayitwithsongs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sayitwithsongs
+//
+//  Created by Haven Barnes on 8/21/17.
+//  Copyright © 2017 Azing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
